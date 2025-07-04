9321e707809278abcb1cb843733c211320a8682d
angularexample <info@angularexample.com>
2025-07-04 18:49:51 -0400

    Add provideHttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
     provideAnimationsAsync(),
+    provideHttpClient(),
     provideRouter(routes),
     provideZoneChangeDetection({ eventCoalescing: true })
   ]
